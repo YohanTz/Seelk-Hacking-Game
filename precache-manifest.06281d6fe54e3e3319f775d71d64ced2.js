0fe03fb9b4cbb8704430cbd3b5b0ae5cffc26ce5
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-07-31 07:01:22 +0200

    Updates

---
+++ precache-manifest.06281d6fe54e3e3319f775d71d64ced2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77513a8baf615ba1adc0efe6e7d8747b",
+    "url": "/Seelk-Hacking-Game/index.html"
+  },
+  {
+    "revision": "18075e58d607d3324c2d",
+    "url": "/Seelk-Hacking-Game/static/css/main.0dc8e177.chunk.css"
+  },
+  {
+    "revision": "b62bb9accdadbb391368",
+    "url": "/Seelk-Hacking-Game/static/js/2.a1751c41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Seelk-Hacking-Game/static/js/2.a1751c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18075e58d607d3324c2d",
+    "url": "/Seelk-Hacking-Game/static/js/main.40716a34.chunk.js"
+  },
+  {
+    "revision": "e0554db61ba08c2752b3",
+    "url": "/Seelk-Hacking-Game/static/js/runtime-main.9d86d77f.js"
+  }
+]);
